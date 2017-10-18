3285e10cb475f16e0c70a4a4a21534e5a451d4d8
Parker Smathers <parkersmathers@yahoo.com>
2017-10-18 13:46:58 -0400

    Add grid js

---
+++ js/grid.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+  $('.grid.active .card').each(function () {
+    var cat = $(this).attr('class').split(' ').shift()
+    var grid = $('.grid')
+    var landing = $('.landing')
+    var link
+
+    $(this).on( {
+
+      'mouseenter': function(e) {
+        e.preventDefault()
+        cards = grid.children().not('.' + cat + '')
+        cards.addClass('faded')
+        link = landing.find('.' + cat + '.f-b')
+        console.log(link);
+        link.addClass('current')
+      },
+      'mouseleave': function (e) {
+        e.preventDefault()
+        cards.removeClass('faded')
+      },
+      'click': function (e) {
+        e.preventDefault()
+        // landing.children('h1').addClass('hidden')
+        // cards.children().addClass('active')
+        // $(this).addClass('active')
+      }
+    })
+  })
+})
