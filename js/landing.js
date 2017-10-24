e05c35b524958fe25fdf10d5a53b6121af92f290
Parker <parker@Mikes-MBP.home>
2017-10-24 15:53:55 -0400

    Changes to landing html and js

--- js/landing.js
+++ js/landing.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-  $('.landing.active a').each(function () {
-    var link = $(this)
+  $('h1').children('span').each(function () {
+    var link = $(this).find('a')
     var href = link.attr('href')
     var cards = $('.grid').find('.' + href + '')
 
@@ -9,20 +9,26 @@ $(document).ready(function () {
 
       'mouseenter': function(e) {
         e.preventDefault()
+        e.stopPropagation()
+        link.addClass('current z2')
         cards.addClass('current z1')
         $('.landing').addClass('z2')
       },
       'mouseleave': function (e) {
         e.preventDefault()
+        e.stopPropagation()
+        link.removeClass('current z2')
         cards.removeClass('current z1')
         $('.landing').removeClass('z2')
       },
       'click': function (e) {
         e.preventDefault()
+        e.stopPropagation()
         $('.landing').removeClass('active').addClass('hidden')
         cards.removeClass('current z1').addClass('active z1')
-        link.addClass('active z2')
+        link.removeClass('current').addClass('active z2')
+        $('a').removeClass('hot')
       }
-    })
+    }, '.hot')
   })
 })
