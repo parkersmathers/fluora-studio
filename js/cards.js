d6a09b6aa9271f6ad1f158c5578ed799936af650
Parker <parker@Mikes-MBP.home>
2017-10-25 17:14:55 -0400

    configure slider routes

--- js/cards.js
+++ js/cards.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
   $('.grid').on( {
 
     'mouseover': function (e) {
-      e.preventDefault()
-      e.stopPropagation()
+      // e.preventDefault()
+      // e.stopPropagation()
       $(this).find('img').css('opacity', 1)
     },
 
     'mouseout': function (e) {
-      e.preventDefault()
-      e.stopPropagation()
+      // e.preventDefault()
+      // e.stopPropagation()
       $(this).find('img').removeAttr('style')
     }
   }, '.card.active')
