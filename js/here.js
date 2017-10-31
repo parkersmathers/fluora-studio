24359ca72f0e983b1287b0cc598a1a8154719e52
Parker <parkersmathers@yahoo.com>
2017-10-31 12:03:24 -0400

    Add listener handler to here link

--- js/here.js
+++ js/here.js
@@ -1,3 +1,17 @@
-// Add listener and handler to 'here' link for
-// mouseenter/leave: toggle shadow grid behind landing text
-// update css
+$(document).ready(function () {
+
+  $('.samples').on( {
+
+    'mouseenter': function (e) {
+      $('.grid').css('visibility', 'visible')
+      $('.card-content').css('background-color', 'rgba(170, 170, 170, .2)')
+      $('.card-image').css('opacity', 0)
+    },
+
+    'mouseleave': function (e) {
+      $('.grid').removeAttr('style')
+      $('.card-content').removeAttr('style')
+      $('.card-image').removeAttr('style')
+    }
+  })
+})
