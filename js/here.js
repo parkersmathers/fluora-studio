7b28e654e10d4a149e32470fb911af58c594caaa
Parker <parkersmathers@yahoo.com>
2017-11-02 08:38:29 -0400

    Tweak to background-color contact and here

--- js/here.js
+++ js/here.js
@@ -3,12 +3,16 @@ $(document).ready(function () {
   $('.samples').on( {
 
     'mouseenter': function (e) {
+      e.preventDefault()
+      e.stopPropagation()
       $('.grid').css('visibility', 'visible')
-      $('.card-content').css('background-color', 'rgba(170, 170, 170, .2)')
+      $('.card-content').css('background-color', 'rgba(170, 170, 170, 0.2)')
       $('.card-image').css('opacity', 0)
     },
 
     'mouseleave': function (e) {
+      e.preventDefault()
+      e.stopPropagation()
       $('.grid').removeAttr('style')
       $('.card-content').removeAttr('style')
       $('.card-image').removeAttr('style')
