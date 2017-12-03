2ddf5a730be0985e6c8e5225f79367b77c7be294
Parker <parkersmathers@yahoo.com>
2017-12-03 12:17:34 -0500

    Implement touchstart for landing

--- js/landing.js
+++ js/landing.js
@@ -1,21 +1,65 @@
 $(document).ready(function () {
+  var touchCountLanding = 1
+  var target
 
-  $('h1').children('span').each(function () {
+  $('h1 span').each(function () {
     var button = $(this)
     var link = button.find('a')
     var href = link.attr('href')
     var cards = $('.grid').find('.' + href + '')
 
+    function handleOneTouch(e) {
+      // console.log('one');
+      // console.log(e);
+      // console.log(target);
+      if (target && (target !== e.target)) {
+        $(target).trigger('mouseout')
+        target = e.target
+        $(target).trigger('mouseover')
+      } else {
+        target = e.target
+        $(target).trigger('mouseover')
+      }
+      touchCountLanding++
+    }
+
+    function handleTwoTouches(e) {
+      // console.log('two');
+      // console.log(e);
+      // console.log(target);
+      if (e.target === target) {
+        $(target).trigger('click')
+        target = undefined
+        touchCountLanding--
+      } else {
+        $(target).trigger('mouseout')
+        target = e.target
+        $(target).trigger('mouseover')
+      }
+    }
+
     $(this).on( {
 
-      'mouseenter': function(e) {
+      'touchstart': function (e) {
+        e.preventDefault()
+        switch (touchCountLanding) {
+          case 1: handleOneTouch(e); break;
+          case 2: handleTwoTouches(e); break;
+          default: console.log('not supported'); break;
+        }
+      }
+    })
+
+    $(this).on( {
+
+      'mouseover': function (e) {
         e.preventDefault()
         e.stopPropagation()
         button.addClass('current z2')
         cards.addClass('current z1')
         $('.landing').addClass('z2')
       },
-      'mouseleave': function (e) {
+      'mouseout': function (e) {
         e.preventDefault()
         e.stopPropagation()
         button.removeClass('current z2')
@@ -31,5 +75,17 @@ $(document).ready(function () {
         $('a').removeClass('hot')
       }
     }, '.hot')
+
+    $('a.taphover').on('touchstart', function (e) {
+      var link = $(this); //preselect the link
+      if (link.hasClass('hover')) {
+        return true;
+      } else {
+        link.addClass('hover');
+        $('a.taphover').not(this).removeClass('hover');
+        e.preventDefault();
+        return false; //extra, and to make sure the function has consistent return points
+      }
+    });
   })
 })
