7d2d54d0bdd7e74b50cec081ddeabf7295251422
Parker <parkersmathers@yahoo.com>
2017-12-25 14:30:42 -0500

    begin loadContent fun

--- public/js/Dynamic.js
+++ public/js/Dynamic.js
@@ -1,1 +1,24 @@
-var nav = document.getElementById('.')
+$('nav').on('click', 'a', function () {
+  _href = $(this).attr('href')
+  history.pushState(null, null, href)
+  loadContent()
+})
+
+var $main = $('#main'),
+    $page = $('#page'),
+    baseHeight = 0
+
+$page.height($page.height())
+baseHeight = $page.height() - $main.height()
+
+function loadContent(href) {
+
+  $main
+    .find('section.active')
+    .fadeOut(200, function () {
+      $main
+        .hide()
+        .load(href + )
+    })
+
+}
