24ab9a8d1c8344a37cffe8587e43499b88f4425d
parkersmathers <parkersmathers@yahoo.com>
2018-05-28 11:58:30 -0400

    revise rollover event listener

--- public/assets/js/rollover.js
+++ public/assets/js/rollover.js
@@ -4,9 +4,10 @@ function highlight(e) {
   var category = e.target.classList[0]
   var matches = links.filter(link => link.classList[0] === category);
   matches.map((item) => {
-    item.classList.add('current')
+    item.classList.toggle('current')
   });
 }
 
 var container = document.querySelector('#container')
-container.addEventListener('click', highlight, false)
+container.addEventListener('mouseover', highlight, false)
+container.addEventListener('mouseout', highlight, false)
