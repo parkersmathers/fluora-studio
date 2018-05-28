fce0a3f6864672ddd2243e6c34a9afffdacc0f4c
parkersmathers <parkersmathers@yahoo.com>
2018-05-28 16:20:03 -0400

    add grayhighlighter to rollover event

--- public/assets/js/rollover.js
+++ public/assets/js/rollover.js
@@ -1,13 +1,30 @@
-function highlight(e) {
-  e.preventDefault()
-  var links = Array.from(document.querySelectorAll('.rollover a'))
-  var category = e.target.classList[0]
-  var matches = links.filter(link => link.classList[0] === category);
-  matches.map((item) => {
-    item.classList.toggle('current')
-  });
-}
+function rollover() {
+  var container = document.querySelector('#container')
+  var links = Array.from(container.querySelectorAll('.rollover a'))
 
-var container = document.querySelector('#container')
-container.addEventListener('mouseover', highlight, false)
-container.addEventListener('mouseout', highlight, false)
+  function highlight(e) {
+    e.preventDefault()
+    var category = e.target.classList[0]
+    var matches = links.filter(link => link.classList[0] === category)
+    matches.map((item) => {
+      item.classList.toggle('current')
+    })
+  }
+  container.addEventListener('mouseover', highlight, false)
+  container.addEventListener('mouseout', highlight, false)
+
+  var here = container.querySelector('.all-gray')
+  var cards = Array.from(container.querySelectorAll('.card'))
+
+  function grayHighlight(e) {
+    cards.map(item => {
+      Array.from(item.children).map(child => {
+        child.classList.toggle('all-gray')
+      })
+    })
+  }
+  here.addEventListener('mouseover', grayHighlight, false)
+  here.addEventListener('mouseout', grayHighlight, false)
+
+}
+rollover()
