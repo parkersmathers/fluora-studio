244eb7f2dc1d0e94810c0046439a01eb9b63a3ea
parkersmathers <parkersmathers@yahoo.com>
2018-06-08 15:03:19 -0400

    change nav -> header

--- public/assets/js/reveal.js
+++ public/assets/js/reveal.js
@@ -1,13 +1,15 @@
-var currentCards = document.querySelectorAll('#grid.hidden .card.current')
+(function() {
+  const currentCards = document.querySelectorAll('#grid.hidden .card.current')
 
-function reveal(e) {
-  e.preventDefault()
-  e.stopPropagation()
-  var target = e.currentTarget
-  Array.from(target.children).map(item => item.classList.toggle('current'))
-}
+  function reveal(e) {
+    e.preventDefault()
+    e.stopPropagation()
+    let target = e.currentTarget
+    Array.from(target.children).map(item => item.classList.toggle('current'))
+  }
 
-Array.from(currentCards).map(item => {
-  item.addEventListener('mouseover', reveal, false)
-  item.addEventListener('mouseout', reveal, false)
-})
+  Array.from(currentCards).map(card => {
+    card.addEventListener('mouseover', reveal, false)
+    card.addEventListener('mouseout', reveal, false)
+  })
+})()
